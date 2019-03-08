c225a57fc9671f9024a2b79faa12cd291eaaf1f1
RLst <letony@icloud.com>
2019-03-08 12:54:48 +1100

    PhysicsMaterial implementation done

---
+++ BasicPhysicsEngine/BasicPhysicsEngine/PhysicsMaterial.cpp
@@ -0,0 +1,69 @@
+#include "PhysicsMaterial.h"
+
+
+float PhysicsMaterial::getDensityFromMaterial(const eMaterial material)
+{
+	switch (material)
+	{
+	case eMaterial::HYDROGEN:		return 0.0898f;			break;
+	case eMaterial::HELIUM:			return 0.179f;			break;
+	case eMaterial::AEROGRAPHITE:	return 0.2f;			break;
+	case eMaterial::AIR:			return 1.2f;			break;
+	case eMaterial::TUNGSTEN_HEXAFLOURIDE: return 12.4f;	break;
+	case eMaterial::STYROFOAM:		return 75;				break;
+	case eMaterial::CORK:			return 240;				break;
+	case eMaterial::PINE:			return 373;				break;
+	case eMaterial::LITHIUM:		return 535;				break;
+	case eMaterial::WOOD:			return 700;				break;
+	case eMaterial::ICE:			return 916.7f;			break;
+	case eMaterial::WATER:			return 1000;			break;
+	case eMaterial::SALT_WATER:		return 1030;			break;
+	case eMaterial::PLASTICS:		return 1175;			break;
+	case eMaterial::MAGNESIUM:		return 1740;			break;
+	case eMaterial::CONCRETE:		return 2400;			break;
+	case eMaterial::DIAMOND:		return 3500;			break;
+	case eMaterial::VANADIUM:		return 6100;			break;
+	case eMaterial::BRASS:			return 8600;			break;
+	case eMaterial::COPPER:			return 8940;			break;
+	case eMaterial::SILVER:			return 10500;			break;
+	case eMaterial::LEAD:			return 11340;			break;
+	case eMaterial::RHODIUM:		return 12410;			break;
+	case eMaterial::MERCURY:		return 13546;			break;
+	case eMaterial::TUNGSTEN:		return 19300;			break;
+	case eMaterial::GOLD:			return 19320;			break;
+	case eMaterial::PLATINUM:		return 21450;			break;
+	case eMaterial::IRIDIUM:		return 22420;			break;
+	case eMaterial::OSMIUM:			return 22570;			break;
+	default:						return -1;
+	}
+}
+
+PhysicsMaterial::PhysicsMaterial(float friction, float elasticity, float density) :
+	friction(friction), elasticity(elasticity), m_density(density)
+{
+	m_material = eMaterial::NIL;
+}
+
+PhysicsMaterial::PhysicsMaterial(float friction, float elasticity, eMaterial material) :
+	friction(friction), elasticity(elasticity), m_material(material)
+{
+	m_density = getDensityFromMaterial(material);
+}
+
+void PhysicsMaterial::setMaterial(const eMaterial material)
+{
+	m_material = material;
+	m_density = getDensityFromMaterial(material);
+}
+
+eMaterial PhysicsMaterial::getMaterial() const
+{
+	return m_material;
+}
+
+void PhysicsMaterial::setDensity(const float density)
+{
+	//Custom density value will unset material (simplistic)
+	m_material = eMaterial::NIL;
+	m_density = density;
+}
--- BasicPhysicsEngine/BasicPhysicsEngine/PhysicsMaterial.h
+++ BasicPhysicsEngine/BasicPhysicsEngine/PhysicsMaterial.h
@@ -1,13 +1,60 @@
 #pragma once
 
+enum class eMaterial
+{
+	NIL,
+	HYDROGEN,
+	HELIUM,
+	AEROGRAPHITE,
+	AIR,
+	TUNGSTEN_HEXAFLOURIDE,
+	STYROFOAM,
+	CORK,
+	PINE,
+	LITHIUM,
+	WOOD,
+	ICE,
+	WATER,
+	SALT_WATER,
+	PLASTICS,
+	MAGNESIUM,
+	CONCRETE,
+	DIAMOND,
+	VANADIUM,
+	BRASS,
+	COPPER,
+	SILVER,
+	LEAD,
+	RHODIUM,
+	MERCURY,
+	TUNGSTEN,
+	GOLD,
+	PLATINUM,
+	IRIDIUM,
+	OSMIUM,
+	MATERIAL_COUNT
+};
 
 class PhysicsMaterial
 {	//Super basic physics material class
-public:
-	float friction = 0.4f;
-	float elasticity = 0.9f;
-	PhysicsMaterial() = default;
-	PhysicsMaterial(float friction, float elasticity) : friction(friction), elasticity(elasticity) {}
-	~PhysicsMaterial();
-};
+private:
+	eMaterial	m_material;
+	float		m_density = 1000;
 
+private:
+	float		getDensityFromMaterial(const eMaterial material);
+
+public:
+	float		friction = 0.4f;
+	float		elasticity = 0.9f;
+
+	PhysicsMaterial(float friction, float elasticity, float density = 1000);
+	PhysicsMaterial(float friction, float elasticity, eMaterial material);
+	~PhysicsMaterial() {}
+
+	void		setMaterial(const eMaterial material);
+	eMaterial	getMaterial() const;
+
+	void		setDensity(const float density);
+	float		getDensity() const { return m_density; }
+};
